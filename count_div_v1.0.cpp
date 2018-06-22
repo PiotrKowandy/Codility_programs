d963ba59a31c5f6f9ab45a73274f6bbb9bfaf45f
Piotr Kowańdy <33897068+PiotrKowandy@users.noreply.github.com>
2018-06-22 12:08:58 +0200

    Add files via upload

---
+++ count_div_v1.0.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Piotr Kowañdy
+Description: Count div
+Version: 1.0
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int solution (int A, int B, int K){
+    int found=0;
+    int start_position=0;
+
+    if (A <= B && K <=B && K >0){
+        for (int i=A; i<= B; i++){
+            if (i%K == 0){
+                start_position = i;
+                break;
+            }
+        }
+        for (int i=start_position; i <= B; i+=K) found++;
+    }
+
+    return found;
+}
+
+
+int main (int argc, char *argv[]){
+    int found = solution(6, 11, 2);
+    cout << "Count div is: " << found << "\n";
+    return 0;
+}
