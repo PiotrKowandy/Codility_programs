cb85464cb7d9c4cb6c248ee093ac830f726590a7
Piotr Kowańdy <33897068+PiotrKowandy@users.noreply.github.com>
2019-02-22 15:56:34 +0100

    Update binary_gap_2nd.cpp

--- binary_gap_2nd.cpp
+++ binary_gap_2nd.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 
 using std::cout;
-using std::endl;
 
 bool non_power(int N){
     return (N && !(N & (N-1))) == true ? true : false;
 }
 
 int solution (int N){
+    
     int found=0, temp=0;
     bool gap_begin=false;
 
     if (!(non_power(N)) && !(non_power(N+1))){
-        while (N != 0){
-            cout << " " << N%2 << " ";
-            if ( N%2 == 1){
+        
+        while (N != 0){            
+            cout << " " << N%2 << " ";            
+            if ( N%2 == 1){                
                 if (temp>found){
                     found = temp;
                     temp=0;
-                }
+                }                
                 temp=0;
                 gap_begin=true;
             }
             else if (gap_begin) temp++;
             N/=2;
         }
+        
     }
+    
     return found;
 }
 
