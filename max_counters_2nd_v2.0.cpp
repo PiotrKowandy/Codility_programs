8a89f7c0cbfa83b0057f33372b96706023f94576
Piotr Kowańdy <33897068+PiotrKowandy@users.noreply.github.com>
2019-02-28 21:12:35 +0100

    Add files via upload

---
+++ max_counters_2nd_v2.0.cpp
@@ -0,0 +1,52 @@
+/*
+Author: Piotr Kowañdy
+Description: Max counters
+Version: 2.0
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+void show_vector(vector<int> &A){
+    int length = A.size();
+    cout << "\n\n";
+    for (int a=0; a< length; a++) cout << " " << A[a] << " ";
+    cout << "\n\n";
+}
+
+vector<int> solution(int N, vector<int> &A){
+
+    std::vector<int> vec_found(N);
+    int counter=0, length=A.size();
+
+    while(counter < length){
+        if (A[counter] <= N && A[counter]>0) vec_found[A[counter]-1]++;
+        else std::fill(vec_found.begin(), vec_found.end(), *(max_element(vec_found.begin(), vec_found.end())));
+        counter++;
+    }
+
+    return vec_found;
+}
+
+int main (int argc, char *argv[]){
+
+	vector<int> vect;
+	vect.push_back(3);
+	vect.push_back(4);
+	vect.push_back(4);
+	vect.push_back(6);
+    vect.push_back(1);
+	vect.push_back(4);
+	vect.push_back(4);
+
+	int N=5;
+	vect = solution(N, vect);
+	cout<< "This is our vector: ";
+    show_vector(vect);
+
+	return 0;
+}
