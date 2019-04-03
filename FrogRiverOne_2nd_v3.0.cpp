a8e1a9e76bc334f43398479e0b17c7a5ca56f575
Piotr Kowańdy <33897068+PiotrKowandy@users.noreply.github.com>
2019-04-03 11:52:11 +0200

    Add files via upload

---
+++ FrogRiverOne_2nd_v3.0.cpp
@@ -0,0 +1,58 @@
+/*
+Author: Piotr Kowañdy
+Description: Frog river one
+2nd, Version: 3.0
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>                       //You need to change attitude!;P
+
+using std::cout;
+using std::vector;
+
+void show_vect(vector<int> &A){
+    int length = A.size();
+    for (int i=0; i < length; i++) cout<< A[i] << " ";
+    cout << "\n";
+}
+
+int solution (int X, std::vector<int> &A){
+    int ready2go=0;
+    show_vect(A);
+    std::cout<< "X = " << X << "\n\n";
+    int length = A.size();
+
+    if ((X == 1) && (A[0]== 1)) return 0;
+    else if (length < X) return -1;
+    else{
+        int frog_position=0;
+        bool applicable=false;
+        while ((X != frog_position) || (applicable == false)){
+            applicable=false;
+            for (int i=0; i<length; i++){
+                if (A[i] == frog_position+1){
+                    frog_position++;
+                    if (ready2go<i) ready2go=i;
+                    applicable=true;
+                    break;
+                }
+            }
+            if (applicable ==false) return -1;
+        }
+    }
+
+    return ready2go;
+}
+
+
+
+int main (int argc, char *argv[]){
+
+	vector<int> vect= {1,3,1,4,2,3,5,4};
+	int X=5, found_second=0;
+	found_second = solution(X, vect);
+	cout<< "Time when all leafs will be on place is: " << found_second << "\n";
+
+	return 0;
+}
